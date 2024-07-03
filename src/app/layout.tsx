16cb81f665d27dff897aabc0859846bc64fbf00a
James Nyangada <jamesedwin03@gmail.com>
2024-07-03 11:36:32 +0300

    google console added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning={isDev}>
       <head>
         <meta name="description" content="Islam tv-254 is an online platform that will help you enrich your  islamic knowledge " />
+        <meta name="google-site-verification" content="gkN4sszP7m5KXmTXKQa1cWFio7mibvhnizutWiWxNRI" />
         <link rel="icon" href="/favicon.png" sizes="any" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,500,600,700i&display=swap" />
       </head>
